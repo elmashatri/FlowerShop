b036b1a0aa110b21510ca5e14195029c0431461d
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2024-12-25 01:20:42 +0100

    Login Part

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+include 'connection.php';
+session_start();
+
+if(isset($_POST['submit_btn'])){
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    $select_user = mysqli_query($conn, "SELECT * FROM `users` WHERE `email` = '$email'") or die('Query failed');
+    
+    if(mysqli_num_rows($select_user) > 0){
+        $row = mysqli_fetch_assoc($select_user);
+        
+        if($row['user_type'] == 'admin'){
+            $_SESSION['admin_name'] = $row['name'];
+            $_SESSION['admin_email'] = $row['email'];
+            $_SESSION['admin_id'] = $row['id'];
+            header('location:admin.php');
+        } else if($row['user_type'] == 'user'){
+            $_SESSION['user_name'] = $row['name'];
+            $_SESSION['user_email'] = $row['email'];
+            $_SESSION['user_id'] = $row['id'];
+            header('location:index.php'); 
+        } else {
+            $message[] = 'Incorrect email or password';
+        }
+    } else {
+        $message[] = 'User not found';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@1.10.2/font/bootsrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>User Login Page</title>
+</head>
+<body>
+    <?php
+        if(isset($message)){
+            foreach($message as $message){
+                echo '
+                    <div class="message">
+                        <span>'.$message.'</span>
+                        <i class="bi bi-x-circle" onclick="this.parentElement.remove()"></i>
+                    </div>
+                ';
+            }
+        }
+    ?>
+    
+    <section class="form-container">
+        <form action="" method="post">
+            <h3>Login Now</h3>
+            <input type="email" name="email" placeholder="Enter your email" required>
+            <input type="password" name="password" placeholder="Enter your password" required>
+            <input type="submit" name="submit_btn" class="btn" value="Login Now">
+            <p>Do not have an account? <a href="register.php">Register Now</a></p>
+        </form>
+    </section>
+
+</body>
+</html>

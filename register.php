3d89c90694b6a821d8449d65102c4a3a71784bed
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2024-12-25 21:46:32 +0100

    Register

--- register.php
+++ register.php
@@ -1,8 +1,7 @@
 <?php
-
 include 'connection.php';
 
-if(isset($_POST['submit_btn'])){
+if(isset($_POST['submit_btn'])){  
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
@@ -24,6 +23,7 @@ if(isset($_POST['submit_btn'])){
             mysqli_query($conn, "INSERT INTO `users` (`name`, `email`, `password`) VALUES ('$name', '$email', '$password')") or die('Query failed');
             $message[] = 'Registered successfully';
             header('location: login.php');
+            exit(); 
         }
     }
 }
@@ -35,35 +35,35 @@ if(isset($_POST['submit_btn'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@1.10.2/font/bootsrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>User Registration Page</title>
 </head>
 <body>
     <?php
         if(isset($message)){
-            foreach($message as $message){
+            foreach($message as $msg){  
                 echo '
                     <div class="message">
-                        <span>'.$message.'</span>
+                        <span>'.$msg.'</span>
                         <i class="bi bi-x-circle" onclick="this.parentElement.remove()"></i>
                     </div>
                 ';
             }
         }
     ?>
-    
+
     <section class="form-container">
         <form action="" method="post">
             <h3>Register Now</h3>
             <input type="text" name="name" placeholder="Enter your name" required>
             <input type="email" name="email" placeholder="Enter your email" required>
             <input type="password" name="password" placeholder="Enter your password" required>
-            <input type="password" name="cpassword" placeholder="Reenter your password" required>
-            <input type="submit" name="submit_btn" class="btn" value="Register Now">
+            <input type="password" name="cpassword" placeholder="Re-enter your password" required>
+            <input type="submit" name="submit_btn" class="btn" value="Register Now"> 
             <p>Already have an account? <a href="login.php">Login Now</a></p>
         </form>
     </section>
-
 </body>
 </html>
+

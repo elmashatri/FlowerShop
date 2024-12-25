5cd25691f6931e353c147f763c3ab67923175fd7
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2024-12-25 21:43:34 +0100

    Footer Part - User

---
+++ footer.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="home-contact">
+        <h1>Any Question?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati assumenda rem ab eum aperiam deserunt iusto nihil, natus cumque! Molestias itaque perspiciatis ex cumque delectus quo vitae consequatur tempore amet!</p>
+        <a href="contact.php" class="btn2">Contact Us</a>
+    </section>
+    <footer class="footer">
+        <div class="inner-footer">
+            <div class="card">
+                <h3>About</h3>
+                <ul>
+                    <li>About</li>
+                    <li>Our Difference</li>
+                    <li>Community Matters</li>
+                    <li>Press</li>
+                    <li>Bougs Video</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>Help</h3>
+                <ul>
+                    <li>Flower Care</li>
+                    <li>Help Center</li>
+                    <li>Shipping</li>
+                    <li>Term of Use</li>
+                    <li>Accessibility</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>Local</h3>
+                <ul>
+                    <li>Prishtina</li>
+                    <li>Tirana</li>
+                    <li>Shkupi</li>
+                    <li>Peja</li>
+                    <li>Prizreni</li>
+                    <li>Ferizaj</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h3>Newsletter</h3>
+                <p>Sign Up for the lattest ice offers and exclusive</p>
+                <div class="input-field">
+                    <input type="text" name=" " placeholder="Email Address...">
+                    <i class="bi bi-envelope"></i>
+                </div>
+                <div class="social-links">
+                    <i class="bi bi-instagram"></i>
+                    <i class="bi bi-youtube"></i>
+                    <i class="bi bi-twitter"></i>
+                    <i class="bi bi-behance"></i>
+                    <i class="bi bi-whatsapp"></i>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <p>All Right Reserved</p>
+        </div>
+    </footer>
+</body>
+</html>

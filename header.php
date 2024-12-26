e71da9074881a1851d105a33e31eb50c290787cc
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2024-12-26 23:04:05 +0100

    Header

--- header.php
+++ header.php
@@ -7,6 +7,43 @@ if (!isset($_SESSION['admin_name']) || !isset($_SESSION['admin_email'])) {
     header("Location: login.php");
     exit();
 }
+
+/*Shtimi i produkteve ne wishlist*/
+if (isset($_POST['add_to_wishlist'])) {
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+
+    $wishlist_number = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name='$product_name' AND user_id='$user_id'") or die(mysqli_error($conn));
+    $cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE name='$product_name' AND user_id='$user_id'") or die(mysqli_error($conn));
+    
+    if (mysqli_num_rows($wishlist_number) > 0) {
+        $message[] = 'Product already exists in wishlist';
+    } else if (mysqli_num_rows($cart_number) > 0) {
+        $message[] = 'Product already exists in cart';
+    } else {
+        mysqli_query($conn, "INSERT INTO `wishlist`(`user_id`, `pid`, `name`, `price`, `image`) VALUES ('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die(mysqli_error($conn));
+        $message[] = 'Product successfully added to wishlist';
+    }
+}
+
+/*Shtimi i produkteve ne cartt*/
+if (isset($_POST['add_to_cart'])) {
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+
+    $cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE name='$product_name' AND user_id='$user_id'") or die(mysqli_error($conn));
+    
+    if (mysqli_num_rows($cart_number) > 0) {
+        $message[] = 'Product already exists in cart';
+    } else {
+        mysqli_query($conn, "INSERT INTO `cart`(`user_id`, `pid`, `name`, `price`, `image`) VALUES ('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die(mysqli_error($conn));
+        $message[] = 'Product successfully added to cart';
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -30,12 +67,22 @@ if (!isset($_SESSION['admin_name']) || !isset($_SESSION['admin_email'])) {
                 <a href="contact.php">Contact</a>
             </nav>
             <div class="icons">
-                <i class="bi bi-list" id="menu-btn"></i>
                 <i class="bi bi-person" id="user-btn"></i>
+                <?php
+                    $select_wishlist = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE user_id='$user_id'") or die(mysqli_error($conn));
+                    $wishlist_num_rows = mysqli_num_rows($select_wishlist);
+                ?>
+                <a href="wishlist.php"><i class="bi bi-heart"></i><span>(<?php echo $wishlist_num_rows; ?>)</span></a>
+                <?php
+                    $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id='$user_id'") or die(mysqli_error($conn));
+                    $cart_num_rows = mysqli_num_rows($select_cart);
+                ?>
+                <a href="cart.php"><i class="bi bi-cart"></i><span>(<?php echo $cart_num_rows; ?>)</span></a>
+                <i class="bi bi-list" id="menu-btn"></i>
             </div>
             <div class="user-box">
-                <p>Username: <span><?php echo htmlspecialchars($_SESSION['user_name']); ?></span></p>
-                <p>Email: <span><?php echo htmlspecialchars($_SESSION['user_email']); ?></span></p>
+                <p>Username: <span><?php echo htmlspecialchars($_SESSION['admin_name']); ?></span></p>
+                <p>Email: <span><?php echo htmlspecialchars($_SESSION['admin_email']); ?></span></p>
                 <form method="post" class="logout">
                     <button type="submit" name="logout" class="logout-btn">Log Out</button>
                 </form>
